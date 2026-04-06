9495b5baf34740ce67c1a217871687d1d9d771ae
samira <herrerasami65@gmail.com>
2026-04-06 12:38:51 -0500

    actualizacion del repositorio

---
+++ logica.js
@@ -0,0 +1,42 @@
+let saldo = 1000;
+let saldoActual=0;
+console.log("Hola samira");
+
+function depositar(){
+     let numDeposito = document.getElementById("cantidad de dinero").value;
+     if(numDeposito<=0){
+      alert("error ingrese una cantidad valida a depositar:");  
+
+     }else{
+        saldoActual=parseFloat(saldo) + parseFloat(numDeposito);
+        saldo = saldoActual;
+        alert("tu saldo actual es: " + saldo);
+     }
+
+
+    }
+    function retirar(){
+     let numRetiro = document.getElementById("cantidad de dinero").value;
+     if(numRetiro<=0){
+      alert("error ingrese una cantidad valida a depositar:");  
+
+     }else{
+        saldoActual=saldo-numRetiro;
+        saldo = saldoActual;
+        alert("tu saldo actual es" + saldo);
+     }
+    }
+        
+    function mostrarSaldo(){
+alert("su saldo actual es "+ saldo);
+
+}
+// SE CRERA UNA FUNCION QUE TRAE LO QUE EL USUSARIOP INGRESA MEDIANTE UN INPUT , SE LO HACE MEDIANTE document.getElementById
+//
+function mostrarMensaje(){
+   let mensajeIngresado = document.getElementById("mensaje ingresado").value
+   console.log(mensajeIngresado);
+   
+
+   alert("Hols samira:"+mensajeIngresado);
+}
--- main.js
+++ main.js
@@ -18,9 +18,32 @@ function cambiarColorFondo(){
 }
 // funcion para pdr al usr prc y cantidad
 function CalcularTotal(){
-    let precio = document.getElementById('prescio').value;
+    let precio = document.getElementById('precio').value;/
     let cantidad= document.getElementById('cantidad').value;
-    let total= parseFloat(precio)*parseFloat(cantidad);
+    let sumaproducto=parseFloat(precio)*parseFloat(cantidad);// total del producto 1
+    if(cantidad<10){
+      precioTotal1 = sumaproducto;  // se almacena sin desc
+  
+    } else{
+       let descuento=sumaproducto*0.20;
+       precioTotal1= sumaproducto-descuento;
+    }
+
+    let precio2= document.getElementById('precio2').value;
+    let cantidad2= document.getElementById('cantidad2').value;
+    let total2= parseFloat(precio2)*parseFloat(cantidad2);
+
+    let precio3= document.getElementById('precio3').value;
+    let cantidad3= document.getElementById('cantidad3').value;
+    let total3= parseFloat(precio3)*parseFloat(cantidad3);
+    
+    
+    let preciofinal=precioTotal1+precioTotal2+precioTotal3;
+
+    let iva=preciofinal*0.15;
+
+    let PrecioIva= preciofinal + iva;
+
     alert("el total a pagar es:" + total);
     alert ("precio con iva:"+ (total* 0.15))
 }
